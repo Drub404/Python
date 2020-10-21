315d1cd005dc9e85fae8c3857c06f46398fb378b
Mr. Cat <47589279+Drub404@users.noreply.github.com>
2020-10-21 19:54:31 +0200

    new games! progresss

--- exercises/guess.num.py
+++ exercises/guess.num.py
@@ -1,2 +1,54 @@
-""" Yet to be worked on.
+""" Make a program in which the computer randomly chooses a number between 1 to 10, 1 to 100, or any range. 
+Then give users a hint to guess the number. Every time the user guesses wrong, he gets another clue, and his score gets reduced. 
+The clue can be multiples, divisible, greater or smaller, or a combination of all.
+
+You will also need functions to compare the inputted number with the guessed number, 
+to compute the difference between the two, and to check whether an actual number was inputted or not in this python project.
+
 https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/ """
+
+import random
+import sys
+import time
+
+score = 5
+
+def start_game():
+    print("""Welcome to Guess The Number!
+        I have chosen a random number ranging from 1-10.
+        Every time you guess wrong, your score gets reduced.
+        If you're left with 0 score, the game will stop and output the number.""")
+    num = random.randint(1, 100)
+    guessing(num, score)
+
+def guessing(num, score):
+    if score <= 0:
+        print("Unfortunately, you have no more tries left. The number was {0}".format(num))
+        time.sleep(3)
+        sys.exit
+    else:
+        guess = input("Guess the number:")
+        compare_num(num, guess, score)
+    
+def compare_num(num, guess, score):
+    if guess == "":
+        print("You haven't entered anything.")
+        guessing(num, score)
+    elif guess != num:
+        score -= 1
+        print("Wrong guess.")
+        guessing(num, score)
+    else:
+        print("You guessed the number right! You received {0} score.".format(score))
+        again = str(input("Would you like to play again?:"))
+        play_again(again)
+
+def play_again(again):
+    if again.lower() == "yes":
+        start_game()
+    elif again.lower() == "no":
+        sys.exit("Thanks for playing me! Bye.")
+    else:
+        sys.exit("Invalid input.")
+
+start_game()
--- exercises/madlibs.py
+++ exercises/madlibs.py
@@ -1,4 +1,12 @@
+""" The program design is such that it will ask users to enter a series of inputs that will be considered as a Mad Lib. 
+Mad lib is one of the python projects for beginners. 
+
+The input could be anything, an adjective, a noun, a pronoun, etc. 
+Once all the inputs are entered, the application will take the data and arrange the inputs into a story template form. 
+https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/ """
+
 import time
+import sys
 
 def user_input():
 
@@ -24,5 +32,15 @@ def madlibs_game(animal, number, color):
         user_input()
     else:
         print("You are a {0} that has {1} legs and has the color {2}.".format(animal, number, color))
+        again = str(input("Would you like to play again?:"))
+        play_again(again)
+
+def play_again(again):
+    if again.lower() == "yes":
+        user_input()
+    elif again.lower() == "no":
+        sys.exit("Thanks for playing me! Bye.")
+    else:
+        sys.exit("Invalid input.")
 
 user_input()

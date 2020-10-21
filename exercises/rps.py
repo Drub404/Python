a1f731ab3e875eb16687d6745b7ca7dd5330fa07
Mr. Cat <47589279+Drub404@users.noreply.github.com>
2020-10-21 22:46:07 +0200

    Update rps.py

--- exercises/rps.py
+++ exercises/rps.py
@@ -7,10 +7,3 @@ You can then either give an option of playing again or decide a pre-determined n
 A scorekeeping function will also have to be created which will return the winner at the end.
 
 https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/#Python_Project_Ideas_Beginners_Level """
-
-import random
-import sys
-
-rps = ["Rock", "Paper", "Scissors"]
-
-def make_move():

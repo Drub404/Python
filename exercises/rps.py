3201fb95ae2550e955272145288ea7b243671ca2
Mr. Cat <47589279+Drub404@users.noreply.github.com>
2020-10-21 22:02:00 +0200

    yet to be finished

---
+++ exercises/rps.py
@@ -0,0 +1,16 @@
+""" We again use the random function here. You make a move first and then the program makes one. 
+To indicate the move, you can either use a single alphabet or input an entire string. 
+A function will have to be set up to check the validity of the move.
+
+Using another function, the winner of that round is decided. 
+You can then either give an option of playing again or decide a pre-determined number of moves in advance. 
+A scorekeeping function will also have to be created which will return the winner at the end.
+
+https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/#Python_Project_Ideas_Beginners_Level """
+
+import random
+import sys
+
+rps = ["Rock", "Paper", "Scissors"]
+
+def make_move():

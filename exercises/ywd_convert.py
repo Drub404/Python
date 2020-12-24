bdfb56d80cf15c95005ba1dfe48fbb78ef1498bd
Mr. Cat <47589279+Drub404@users.noreply.github.com>
2020-12-24 15:30:35 +0100

    Delete ywd_convert.py

--- exercises/ywd_convert.py
+++
@@ -1,6 +0,0 @@
-number_of_days = int(input("Enter number of days"))
-number_of_years = int(number_of_days / 365)
-number_of_weeks = int(number_of_days % 365 / 7)
-
-remaining_number_of_days = int(number_of_days % 365 % 7)
-print("Years = {0}, Weeks = {1}, Days {2}".format(number_of_years, number_of_weeks, remaining_number_of_days))
